4bac7c5d714f3e53cc4fbad463ed85e38b197f48
fermin234 <fermin234@hotmail.com>
2023-03-30 12:59:14 -0300

    output css and initial page

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,13 @@
 'use client'
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from "react";
 
 export default function HomePage() {
-  let storage
-  if (process.browser) {
-    storage = JSON.parse(localStorage.getItem("form"))
-  }
+  const router = useRouter()
+  const [storage] = useState(JSON.parse(localStorage.getItem("form")))
+
+
 
   const [results, setResults] = useState({
     correo: "",
@@ -18,8 +18,8 @@ export default function HomePage() {
     Qué_puesto_o_empleo_buscas: "",
     En_qué_tipo_área_o_departamentos_te_interesa_trabajar: "",
     En_qué_tipo_de__empresa_te_ves_desarrollándote_profesionalmente: "",
-    Cómo_te_sientes_en_esta_búsqueda_de_empleo: "",
-    Qué_buscas_en_un_empleo: "",
+    Cómo_te_sientes_en_esta_búsqueda_de_empleo: storage?.datos_personales.Cómo_te_sientes_en_esta_búsqueda_de_empleo.length ? storage.datos_personales.Cómo_te_sientes_en_esta_búsqueda_de_empleo : [],
+    Qué_buscas_en_un_empleo: storage?.datos_personales.Qué_buscas_en_un_empleo.length ? storage.datos_personales.Qué_buscas_en_un_empleo : [],
     Cuánto_tiempo_llevas_desempleado: ""
   })
 
@@ -35,6 +35,21 @@ export default function HomePage() {
     }
   }
 
+  function handleChangeChecbox(e) {
+    const value = e.target.value
+    if (storage.datos_personales[e.target.id].includes(value)) {
+      // const index = results[e.target.id].findIndex(e => e === value)
+      // results[e.target.id].splice(index, 1)
+
+      const index2 = storage.datos_personales[e.target.id].findIndex(e => e === value)
+      storage.datos_personales[e.target.id].splice(index2, 1)
+    } else {
+      results[e.target.id].push(value)
+      storage?.datos_personales[e.target.id].push(value)
+    }
+    localStorage.setItem("form", JSON.stringify(storage))
+  }
+
   useEffect(() => {
     const obj = {
       datos_personales: {
@@ -48,7 +63,7 @@ export default function HomePage() {
         En_qué_tipo_de__empresa_te_ves_desarrollándote_profesionalmente: "",
         Cómo_te_sientes_en_esta_búsqueda_de_empleo: [],
         Qué_buscas_en_un_empleo: [],
-        Cuánto_tiempo_llevas_desempleado: []
+        Cuánto_tiempo_llevas_desempleado: ""
       },
       cv: {
         results: {
@@ -105,14 +120,14 @@ export default function HomePage() {
   return (
     <>
 
-      <h1 class="text-5xl font-extrabold dark:text-white"> Diagnóstico inicial de búsqueda de empleo.<br /><small class="ml-2 font-semibold text-gray-500 dark:text-gray-400">Por  favor  contesta con la mayor honestidad posible sobre tu situación en este momento para poder hacer el mejor diagnóstico posible y darte propuestas solamente con  los servicios que  realmente necesitas. De antemano gracias.</small></h1>
+      <h1 className="text-5xl font-extrabold dark:text-white"> Diagnóstico inicial de búsqueda de empleo.<br /><small className="ml-2 font-semibold text-gray-500 dark:text-gray-400">Por  favor  contesta con la mayor honestidad posible sobre tu situación en este momento para poder hacer el mejor diagnóstico posible y darte propuestas solamente con  los servicios que  realmente necesitas. De antemano gracias.</small></h1>
       <form>
         <div className="grid gap-6 mb-6 md:grid-cols-2">
 
           {/* correo */}
           <div>
             <label htmlFor="correo" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Dirección de correo electrónico *</label>
-            <input onChange={handleChange} type="text" id="correo" name="correo" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={storage?.datos_personales.correo} />
+            <input onChange={handleChange} type="text" id="correo" name="correo" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={storage?.datos_personales.correo} required />
           </div>
 
           {/* Nombre completo */}
@@ -163,32 +178,32 @@ export default function HomePage() {
 
             {/* Motivado */}
             <div className="flex items-center">
-              <input onChange={handleChange} id="Cómo_te_sientes_en_esta_búsqueda_de_empleo" type="checkbox" value="Motivado" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} id="Cómo_te_sientes_en_esta_búsqueda_de_empleo" type="checkbox" value="Motivado" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" defaultChecked={results.Cómo_te_sientes_en_esta_búsqueda_de_empleo.includes("Motivado") ? true : false} />
               <label htmlFor="Motivado" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Motivado (a).</label>
             </div>
 
             {/* Enojado */}
             <div className="flex items-center">
-              <input onChange={handleChange} id="Cómo_te_sientes_en_esta_búsqueda_de_empleo" type="checkbox" value="Enojado" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} defaultChecked={results.Cómo_te_sientes_en_esta_búsqueda_de_empleo.includes("Enojado") ? true : false} id="Cómo_te_sientes_en_esta_búsqueda_de_empleo" type="checkbox" value="Enojado" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="Enojado" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Enojado (a).</label>
             </div>
 
             {/* Desesperado */}
             <div className="flex items-center">
-              <input onChange={handleChange} id="Cómo_te_sientes_en_esta_búsqueda_de_empleo" type="checkbox" value="Desesperado" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} defaultChecked={results.Cómo_te_sientes_en_esta_búsqueda_de_empleo.includes("Desesperado") ? true : false} id="Cómo_te_sientes_en_esta_búsqueda_de_empleo" type="checkbox" value="Desesperado" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="Desesperado" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Desesperado (a)</label>
             </div>
 
             {/* Entusiasta */}
             <div className="flex items-center">
-              <input onChange={handleChange} id="Cómo_te_sientes_en_esta_búsqueda_de_empleo" type="checkbox" value="Entusiasta" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} defaultChecked={results.Cómo_te_sientes_en_esta_búsqueda_de_empleo.includes("Entusiasta") ? true : false} id="Cómo_te_sientes_en_esta_búsqueda_de_empleo" type="checkbox" value="Entusiasta" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="Entusiasta" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Entusiasta.</label>
             </div>
 
             {/* Otro */}
             <div>
               <label htmlFor="otro" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Otro</label>
-              <input onChange={handleChange} name="Cómo_te_sientes_en_esta_búsqueda_de_empleo" type="text" id="Cómo_te_sientes_en_esta_búsqueda_de_empleo" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+              <input name="Cómo_te_sientes_en_esta_búsqueda_de_empleo" type="text" id="Cómo_te_sientes_en_esta_búsqueda_de_empleo" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
           </div>
 
@@ -198,43 +213,43 @@ export default function HomePage() {
 
             {/* Aprender */}
             <div className="flex items-center">
-              <input onChange={handleChange} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Aprender" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} defaultChecked={results.Qué_buscas_en_un_empleo.includes("Aprender") ? true : false} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Aprender" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="Aprender" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Aprender</label>
             </div>
 
             {/* Crecer profesionalmente */}
             <div className="flex items-center">
-              <input onChange={handleChange} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Crecer_profesionalmente" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} defaultChecked={results.Qué_buscas_en_un_empleo.includes("Crecer_profesionalmente") ? true : false} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Crecer_profesionalmente" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="Crecer_profesionalmente" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Crecer profesionalmente</label>
             </div>
 
             {/* Mejor sueldo */}
             <div className="flex items-center">
-              <input onChange={handleChange} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Mejor_sueldo" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} defaultChecked={results.Qué_buscas_en_un_empleo.includes("Mejor_sueldo") ? true : false} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Mejor_sueldo" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="Mejor_sueldo" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Mejor sueldo</label>
             </div>
 
             {/* Mejor ambiente laboral */}
             <div className="flex items-center">
-              <input onChange={handleChange} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Mejor_ambiente_laboral" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} defaultChecked={results.Qué_buscas_en_un_empleo.includes("Mejor_ambiente_laboral") ? true : false} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Mejor_ambiente_laboral" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="Mejor_ambiente_laboral" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Mejor ambiente laboral</label>
             </div>
 
             {/* Mejores prestaciones */}
             <div className="flex items-center">
-              <input onChange={handleChange} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Mejores_prestaciones" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} defaultChecked={results.Qué_buscas_en_un_empleo.includes("Mejores_prestaciones") ? true : false} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Mejores_prestaciones" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="Mejores_prestaciones" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Mejores prestaciones</label>
             </div>
 
             {/* Cambio de giro */}
             <div className="flex items-center">
-              <input onChange={handleChange} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Cambio_de_giro" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} defaultChecked={results.Qué_buscas_en_un_empleo.includes("Cambio_de_giro") ? true : false} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Cambio_de_giro" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="Cambio_de_giro" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Cambio de giro</label>
             </div>
 
             {/* Estabilidad */}
             <div className="flex items-center">
-              <input onChange={handleChange} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Estabilidad" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+              <input onChange={handleChangeChecbox} defaultChecked={results.Qué_buscas_en_un_empleo.includes("Estabilidad") ? true : false} name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" type="checkbox" value="Estabilidad" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
               <label htmlFor="Estabilidad" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Estabilidad</label>
             </div>
 
@@ -242,7 +257,7 @@ export default function HomePage() {
             {/* Otro */}
             <div>
               <label htmlFor="otro" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Otro</label>
-              <input onChange={handleChange} type="text" name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+              <input onChange={handleChangeChecbox} type="text" name="Qué_buscas_en_un_empleo" id="Qué_buscas_en_un_empleo" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
             </div>
           </div>
         </div>
@@ -253,10 +268,6 @@ export default function HomePage() {
           <label htmlFor="Cuánto_tiempo_llevas_desempleado" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">¿Cuánto tiempo llevas desempleado (a)? *</label>
           <input onChange={handleChange} type="text" id="Cuánto_tiempo_llevas_desempleado" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" defaultValue={storage?.datos_personales.Cuánto_tiempo_llevas_desempleado} />
         </div>
-
-        <Link href={"/cv"}>
-          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Siguiente</button>
-        </Link>
       </form>
     </>
   )
