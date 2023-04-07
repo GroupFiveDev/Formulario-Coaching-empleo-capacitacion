fba1ba8f9013bcf7d30993875f27e6ccd437dc39
SantiRosso <santiagorosso@live.com.ar>
2023-04-06 23:46:23 -0300

    Cambio en los títulos

--- app/empleo/page.jsx
+++ app/empleo/page.jsx
@@ -110,7 +110,9 @@ export default function EmpleoPage() {
     <Image width='100' height='100' src="/logo2.png" alt="logo" className="position: fixed top-0 left-0"/>
       <div className="space-y-12">
         <div className="flex flex-col items-center text-center border-gray-900/10">
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-blue-800 md:text-5xl lg:text-1">Empleo</h1>
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-blue-800 md:text-5xl lg:text-1">Búsqueda
+
+de empleo</h1>
           <div className="border-gray-900/10 pb-12">
             {/* pregunta 1 */}
             <div className="mt-10 space-y-10">
