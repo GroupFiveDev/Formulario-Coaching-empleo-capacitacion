1d8dc2c3dcf2fb41bf1bd7b760127e5a20a7545e
Fermín Solaberrieta <101217699+fermin234@users.noreply.github.com>
2023-04-13 12:06:22 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,5 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    screens: {
-      tablet: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
-      desktop: "1280px",
-      // => @media (min-width: 1280px) { ... }
-    },
-    extend: {},
-  },
   plugins: [require("@tailwindcss/forms")],
 };
